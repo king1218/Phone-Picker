b6f2beeb96c1f297fe19fbf735089dcfc5cfb63c
king1218 <abdullah15-12377@diu.edu.bd>
2022-04-19 10:56:08 +0600

    first commit

---
+++ new.js
@@ -0,0 +1,14 @@
+// function Prnt(){
+//     console.log(`Hollo world`);
+// }
+
+// Prnt();
+
+var x=10;
+var y =3;
+ if(x>y){
+     console.log(`${x} is greater`);
+ }
+ else{
+     console.log(`${y} is greater`);
+ }
